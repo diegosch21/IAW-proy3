8ee504f35c0e8bec95cfadccc1f3c026de1cdf53
Diego Schwindt <diego.sch21@gmail.com>
2012-06-01 21:12:45 -0300

    ajax loader.. y backup

---
+++ data/backup.php
@@ -0,0 +1,56 @@
+<?php
+
+$destination = './backup.zip';
+$source = '../';
+
+try {
+	$zip = new ZipArchive();
+	
+	
+    $zip->open($destination, ZIPARCHIVE::CREATE | ZIPARCHIVE::OVERWRITE);
+	
+	if (is_dir($source) === true)
+    {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+		
+        foreach ($files as $file)
+        {
+        	
+            $file = str_replace('\\', '/', $file);
+			
+			if (!stripos($file, '.git')) {
+				
+	            if (is_dir($file) === true)
+	            {
+	                $path = str_replace($source, '', $file . '/');
+				
+	                $zip->addEmptyDir($path);
+					
+	            }
+	            else if (is_file($file) === true)
+	            {
+	            	$path = str_replace($source, '', $file);
+					
+	                $zip->addFile($file,$path);
+	            }
+			}
+        }
+    }
+    else if (is_file($source) === true && !stripos($file, '.git'))
+    {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+    
+	header("Content-type: application/octet-stream");
+	header("Content-disposition: attachment; filename=backup.zip");
+	header ("Content-Length: ".filesize($destination));
+	readfile($destination);
+	unlink($destination);
+	
+	
+} catch(Exception $e){
+	echo $e->getMessage();
+}
+
+
+?>
--- data/upload_img.php
+++ data/upload_img.php
@@ -1,148 +1,133 @@
 <?php
-require_once('../_lib/db.php');
-require_once('../_lib/data.php');
+require_once ('../_lib/db.php');
+require_once ('../_lib/data.php');
 
 try {
 
-session_start();	
+	session_start();
 
-if (!isset($_SESSION['user']) || $_SESSION['user']!= 'admin'){
-	$result['error'] = 'Sesion admin no iniciada';
-}
-else if(!isset($_POST['id']) || $_POST['id']=="" || !is_numeric($_POST['id'])){
-	$result['error'] = 'Falta atributo id';
-}	
-else if(!isset($_POST['class']) || ( $_POST['class']!="artista" && $_POST['class']!="cd" )) {
-	$result['error'] = 'Falta atributo class(artista o img)';
-}	
-else if(!isset($_FILES['file']) || $_FILES['file']==""){
-	$result['error'] = 'Falta archivo';
-}
-else{
-	$file = $_FILES['file'];
-	
-	$db = new DB('../db/iaw_proy3');
-	
-	$id = $_POST['id'];
-	
-	$class = $_POST['class'];
-	
-	if($class == 'artista') {
-		$artistas = $db->query("SELECT id_genero, imagenes FROM artistas WHERE id_artista = $id");
-		$art = $db->getRow($artistas);
-	
-		if(!$art) {
-			//no existe artista
-			$result['error'] = "Artista inexistente con ID=$id";
-		}
-		else {
-			$result['id'] = $id;
-			$resul['class'] = $class;	
-				
-			$imgs = $art['imagenes'];
-			$idGen = $art['id_genero'];
-			$folder = 'img/gen'.$idGen.'/art'.$id;
-			createFolder($folder);
-			$path = saveFile($file,$folder);
-			
-			if($path == 'error') {
-			 	$result['uploaded'] = false;
-		 	}
-			else {
-				$url = 'data/'.$path; 
-			 	$result['uploaded'] = $url;
-				
-				if ($imgs =="") {
-					$imgs = $url;
+	if (!isset($_SESSION['user']) || $_SESSION['user'] != 'admin') {
+		$result['error'] = 'Sesion admin no iniciada';
+	} else if (!isset($_GET['id']) || $_GET['id'] == "" || !is_numeric($_GET['id'])) {
+		$result['error'] = 'Falta atributo id';
+	} else if (!isset($_GET['class']) || ($_GET['class'] != "artista" && $_GET['class'] != "cd")) {
+		$result['error'] = 'Falta atributo class(artista o img)';
+	} else if (!isset($_FILES['file']) || $_FILES['file'] == "") {
+		$result['error'] = 'Falta archivo';
+	} else {
+		$file = $_FILES['file'];
+
+		$db = new DB('../db/iaw_proy3');
+
+		$id = $_GET['id'];
+
+		$class = $_GET['class'];
+
+		if ($class == 'artista') {
+			$artistas = $db -> query("SELECT id_genero, imagenes FROM artistas WHERE id_artista = $id");
+			$art = $db -> getRow($artistas);
+
+			if (!$art) {
+				//no existe artista
+				$result['error'] = "Artista inexistente con ID=$id";
+			} else {
+				$result['id'] = $id;
+				$resul['class'] = $class;
+
+				$imgs = $art['imagenes'];
+				$idGen = $art['id_genero'];
+				$folder = 'img/gen' . $idGen . '/art' . $id;
+				createFolder($folder);
+				$path = saveFile($file, $folder);
+
+				if ($path == 'error') {
+					$result['uploaded'] = false;
+				} else {
+					$url = 'data/' . $path;
+					$result['uploaded'] = $url;
+
+					if ($imgs == "") {
+						$imgs = $url;
+					} else {
+						$imgs .= '|-|' . $url;
+					}
+
+					$res = $db -> execute("UPDATE artistas SET  imagenes = ? WHERE id_artista = ?", array($imgs, $id));
+					if ($res > 0)
+						$result['added'] = true;
+					else {
+						$result['added'] = false;
+					}
 				}
-				else {
-					$imgs .= '|-|'.$url;
-				}
-				
-				$res = $db->execute("UPDATE artistas SET  imagenes = ? WHERE id_artista = ?",array($imgs,$id));
-				if($res > 0)
-					$result['added'] = true;
-				else {
-					$result['added'] = false;
-				}
+
+				$result['listimgs'] = $imgs;
 			}
-			
-			$result['listimgs'] = $imgs;
-		}
-			
-	} elseif($class == 'cd') {
-		$cds = $db->query("SELECT id_artista, imagenes, thumbnail FROM CDs WHERE id_cd = $id");
-		$cd = $db->getRow($cds);
-	
-		if(!$cd) {
-			//no existe artista
-			$result['error'] = "CD inexistente con ID=$id";
-		}
-		else {
-			$result['id'] = $id;
-			$result['class'] = $class;
-			$thumb = (isset($_POST['thumb']) && $_POST['thumb']);
-			
-			if ($thumb) {
-				$col = 'thumbnail';
-				$result['thumbnail'] = true;
-			}
-			else {
-				$col = 'imagenes';
-			}
-	
-			$imgs = $cd[$col];
-			
-			$idArt = $cd['id_artista'];
-			$artistas = $db->query("SELECT id_genero, imagenes FROM artistas WHERE id_artista = $idArt");
-			$art = $db->getRow($artistas);
-			$idGen = $art['id_genero'];
-			
-			$folder = 'img/gen'.$idGen.'/art'.$idArt.'/cd'.$id;
-			createFolder($folder);
-			$path = saveFile($file,$folder);
-			
-			if($path == 'error') {
-			 	$result['uploaded'] = false;
-		 	}
-			else {
-				$url = 'data/'.$path; 
-			 	$result['uploaded'] = $url;
-				
-				if ($imgs =="" || $thumb) {
-					$imgs = $url;
+
+		} elseif ($class == 'cd') {
+			$cds = $db -> query("SELECT id_artista, imagenes, thumbnail FROM CDs WHERE id_cd = $id");
+			$cd = $db -> getRow($cds);
+
+			if (!$cd) {
+				//no existe artista
+				$result['error'] = "CD inexistente con ID=$id";
+			} else {
+				$result['id'] = $id;
+				$result['class'] = $class;
+				$thumb = (isset($_GET['thumb']) && $_GET['thumb']);
+
+				if ($thumb) {
+					$col = 'thumbnail';
+					$result['thumbnail'] = true;
+				} else {
+					$col = 'imagenes';
 				}
-				else {
-					$imgs .= '|-|'.$url;
+
+				$imgs = $cd[$col];
+
+				$idArt = $cd['id_artista'];
+				$artistas = $db -> query("SELECT id_genero, imagenes FROM artistas WHERE id_artista = $idArt");
+				$art = $db -> getRow($artistas);
+				$idGen = $art['id_genero'];
+
+				$folder = 'img/gen' . $idGen . '/art' . $idArt . '/cd' . $id;
+				createFolder($folder);
+				$path = saveFile($file, $folder);
+
+				if ($path == 'error') {
+					$result['uploaded'] = false;
+				} else {
+					$url = 'data/' . $path;
+					$result['uploaded'] = $url;
+
+					if ($imgs == "" || $thumb) {
+						$imgs = $url;
+					} else {
+						$imgs .= '|-|' . $url;
+					}
+
+					$res = $db -> execute("UPDATE CDs SET  $col = ? WHERE id_cd = ?", array($imgs, $id));
+					if ($res > 0)
+						$result['added'] = true;
+					else {
+						$result['added'] = false;
+					}
 				}
-				
-				$res = $db->execute("UPDATE CDs SET  $col = ? WHERE id_cd = ?",array($imgs,$id));
-				if($res > 0)
-					$result['added'] = true;
-				else {
-					$result['added'] = false;
-				}
+
+				$result[$col] = $imgs;
+
 			}
-			
-			$result[$col] = $imgs;
-			
 
 		}
-				
+
+		$db -> disconnect();
 	}
-	
-	
-	$db->disconnect();
+
+} catch(Exception $e) {
+	$result['error'] = $e -> getMessage();
 }
 
-} catch(Exception $e){
-	$result['error'] = $e->getMessage();
-}
-
-if(isset($result['error']))	
+if (isset($result['error']))
 	echo json_encode($result);
-else if(isset($result))
+else if (isset($result))
 	echo json_encode($result);
-
-
 ?>
